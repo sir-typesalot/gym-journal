e02f1f744ab5f00a1e93fe4d787c8c487cb6bc1f
Radhe Pokhrel <rshyampokhrel@gmail.com>
2023-02-01 23:00:54 -0500

    Working on base template

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -3,7 +3,11 @@ from flask import Flask
 from .private import internal
 from .public import external
 
-webapp = Flask(__name__)
+webapp = Flask(
+    __name__, 
+    template_folder='../frontend/templates',
+    static_folder='../frontend/static'
+)
 webapp.secret_key = '93843hserj39sfsw3'
 webapp.register_blueprint(external)
 webapp.register_blueprint(internal)
--- app/routes/public.py
+++ app/routes/public.py
@@ -1,10 +1,15 @@
-from flask import request, Blueprint
+from flask import request, Blueprint, render_template
 
 external = Blueprint('public', __name__)
 
+variables = {
+    'title': 'Home',
+    'footer_text': 'Gym-Journal'
+}
+
 @external.route('/')
 def public():
-    return "helllo public"
+    return render_template('home.html', vars=variables)
 
 # Login
 # Main Page
