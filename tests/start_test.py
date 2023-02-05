aea9ce4735bd7091ef495377850044da958c22d7
Radhe Pokhrel <rshyampokhrel@gmail.com>
2023-02-04 23:32:40 -0500

    Trying out pytest

--- tests/start_test.py
+++ tests/start_test.py
@@ -1,6 +1,8 @@
 
-def test_always_passes():
-    assert True
+# content of test_sample.py
+def inc(x):
+    return x + 1
 
-def test_always_fails():
-    assert False
+
+def test_answer():
+    assert inc(3) == 4
