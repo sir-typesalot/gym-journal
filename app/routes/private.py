399c7a54234f180af1925d79b209613df3c611d3
Radhe Pokhrel <rshyampokhrel@gmail.com>
2023-02-17 23:33:32 -0500

    Working on dashboard layout

--- app/routes/private.py
+++ app/routes/private.py
@@ -1,7 +1,17 @@
-from flask import request, Blueprint
+from flask import render_template, Blueprint
 
 internal = Blueprint('private', __name__)
+
+variables = {
+    'title': 'Home',
+    'footer_text': 'Gym-Journal',
+    'cache': 1
+}
 
 @internal.route('/private')
 def private():
     return "helllo"
+
+@internal.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html', vars=variables)
